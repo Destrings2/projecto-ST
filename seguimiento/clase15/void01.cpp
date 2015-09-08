1cce62b0e75a0ccb3af122a9bd72d299bb32317f
lparedes <lparedes@5ef5c71b-588b-46d0-b40a-ce14aaaccaf2>
2015-09-08 13:52:08 +0000

    Clase 15

---
+++ seguimiento/clase15/void01.cpp
@@ -0,0 +1,24 @@
+
+/*
+ * fichero: void01.cpp
+ * compilar: $ g++ -o void01 void01.cpp
+ * ejecutar: $ ./void01
+ * proposito: Ejemplo de tipo void
+ */
+#include <iostream>
+
+void funcion();
+
+int
+main(void) {
+
+  return 0;
+}
+
+void funcion() {
+  
+  std::cout << "Esto es un efecto colateral" << std::endl;
+}
+
+
+ 
